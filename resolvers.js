2a9ea761cbd4c3be46631f66ae102c00f130c995
shanjairaj7 <shanjairajvlogs@gmail.com>
2021-08-14 11:21:49 +0530

    API token changed

--- resolvers.js
+++ resolvers.js
@@ -4,17 +4,19 @@ const resolvers = {
   Mutation: {
     sendEmail: async (_, args, context) => {
       const { input } = args;
-      sendgridMail.setApiKey(process.env.SENDGRID_API_KEY);
+      sendgridMail.setApiKey(
+        "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"
+      );
       const message = {
-        to: "shanjairajdev@gmail.com",
-        from: "rajsuthan666@gmail.com",
+        to: "rajsuthan666@gmail.com",
+        from: "shanjairajdev@gmail.com",
         subject: "Email from Contact form",
         text: `Email from ${input.from}, Message - ${input.message}`,
       };
 
       try {
         const email = await sendgridMail.send(message);
-        console.log(email);
+
         return {
           success: true,
           message: "Successfully sent email",
